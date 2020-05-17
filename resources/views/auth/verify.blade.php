fb9d66fa55cb17be70ca9a16825f4ecdb3283227
Ayoamaro <48093528+Ayoamaro@users.noreply.github.com>
2020-05-17 18:53:25 +0100

    Add files via upload

--- resources/views/auth/verify.blade.php
+++ resources/views/auth/verify.blade.php
@@ -5,17 +5,17 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Verify Your Email Address') }}</div>
+                <div class="card-header">{{ __('Verificación de correo electrónico') }}</div>
 
                 <div class="card-body">
                     @if (session('resent'))
                         <div class="alert alert-success" role="alert">
-                            {{ __('A fresh verification link has been sent to your email address.') }}
+                            {{ __('Acabamos de enviar un correo de confirmación') }}
                         </div>
                     @endif
 
-                    {{ __('Before proceeding, please check your email for a verification link.') }}
-                    {{ __('If you did not receive the email') }}, <a href="{{ route('verification.resend') }}">{{ __('click here to request another') }}</a>.
+                    {{ __('Antes de proceder, por favor confirme su cuenta con el correo de verificación') }}
+                    {{ __('Si no has recivido el correo') }}, <a href="{{ route('verification.resend') }}">{{ __('Por favor, haga click aquí') }}</a>.
                 </div>
             </div>
         </div>
