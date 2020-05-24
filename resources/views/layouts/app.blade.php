14346281dd6191189c9794aae87cf40d36d20138
Ayoamaro <48093528+Ayoamaro@users.noreply.github.com>
2020-05-24 15:46:52 +0100

    Añadido scripts en la cabecera

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,6 +13,10 @@
         <link rel="stylesheet" href="{{ asset('css/app.css') }}">
         <!-- CSS File -->
         <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <!-- Scripts jQuery, Popper.js, Bootstrap JS -->
+        <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     </head>
 
     <!-- INICIO. Cuerpo de página -->
@@ -99,11 +103,6 @@
         </div>
     </section>
     <!-- FINAL. Sección de Multimedia -->
-
-      <!-- Scripts jQuery, Popper.js, Bootstrap JS -->
-      <script src="https://code.jquery.com/jquery-3.4.1.js"></script>
-      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
     </body>
     <!-- FINAL. Cuerpo de página -->
 
